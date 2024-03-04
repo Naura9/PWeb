a9fd17c491e86ffa9dfe60890c7e646780101285
Naura Haidar Ramadhona Arzin <zadoai.naura@gmail.com>
2024-03-04 12:59:22 +0700

    Selesai praktikum 1, 2, 3

---
+++ Pertemuan4/operator.php
@@ -0,0 +1,14 @@
+<?php
+//membuat varibael a dan b untuk mencoba penggunaan operator pada file operator.php//
+$a = 10;
+$b = 5;
+
+$hasilTambah = $a + $b;
+$hasilKurang = $a - $b;
+$hasilKali = $a * $b;
+$hasilBagi = $a / $b;
+$sisaBagi = $a % $b;
+$pangkat = $a ** $b;
+
+echo 
+?>
